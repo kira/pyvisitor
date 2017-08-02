a2b25b83844dc49842b6f4c9ec28563458ce0738
Luckner Jr. Jean-Baptiste <lucknerjb@gmail.com>
2017-08-02 11:32:56 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='pyvisitor',
+    version='1.0',
+    description='An implementation of the visitor pattern for Python',
+    author='Curtis Schlak',
+    author_email='none@none.com',
+    packages=['pyvisitor'],  #same as name
+)
